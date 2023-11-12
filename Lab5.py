7411026d1dcb125c06dd471da814786c471b1a01
TanyaKiseliova <thinjelly1995@gmail.com>
2023-11-12 17:32:11 +0300

    lab 5 tasks 1.1, 1.2, 3.1, 3.2

--- Lab5.py
+++ Lab5.py
@@ -94,7 +94,51 @@ def task3_1():
     plt.show()
 
 def task3_2():
-    pass
+    fig, axes= plt.subplots(2,3, figsize=(15, 10))
+
+    x=np.linspace(4, 20, 10)
+    y=np.linspace(4, 20, 10)
+    X, Y=np.meshgrid(x,y)
+
+    Z1=X**0.25+Y**0.25
+    axes[0, 0].contour(X, Y, Z1, levels=20, colors='k')
+    axes[0, 0].set_title("z = x^0.25+y^0.25")
+    axes[0,0].set_xlabel('X')
+    axes[0,0].set_ylabel('Y')
+
+    x = np.linspace(-2, 2, 100)
+    y = np.linspace(-2, 2, 100)
+    X, Y = np.meshgrid(x, y)
+
+    Z2 = X ** 2 - Y ** 2
+    axes[0, 1].contour(X, Y, Z2, levels=20, colors='k')
+    axes[0, 1].set_title('z = x^2 - y^2')
+    axes[0, 1].set_xlabel('X')
+    axes[0, 1].set_ylabel('Y')
+
+    Z3 = 2*X+3*Y
+    axes[0, 2].contour(X, Y, Z3, levels=20, colors='k')
+    axes[0, 2].set_title('z = 2x + 3y')
+    axes[0, 2].set_xlabel('X')
+    axes[0, 2].set_ylabel('Y')
+
+    Z4 = X ** 2 + Y ** 2
+    axes[1, 0].contour(X, Y, Z4, levels=20, colors='k')
+    axes[1, 0].set_title('z = x^2 + y^2')
+    axes[1, 0].set_xlabel('X')
+    axes[1, 0].set_ylabel('Y')
+
+    Z5 = 2+2*X+2*Y-X**2-Y**2
+    axes[1, 1].contour(X, Y, Z5, levels=20, colors='k')
+    axes[1, 1].set_title('z = 2 + 2x + 2y - x^2 - y^2')
+    axes[1, 1].set_xlabel('X')
+    axes[1, 1].set_ylabel('Y')
+
+    axes[1,2].axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
 def isInt(value):
     try:
         int(value)
